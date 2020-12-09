2109fc2120aba8ed16f277613385c0a53cda31a7
darioalejandrovv <darioalejandrovv@gmail.com>
2020-12-09 09:39:59 -0500

    uppp

---
+++ precache-manifest.2f88a28e7be8cf3b3ecc760b15016b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e36b6ae6048d10292756301abac6a282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d88d1de1818e829490e7",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d88d1de1818e829490e7",
+    "url": "/static/js/2.aba358b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aba358b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3c23b0fd20e607c9068",
+    "url": "/static/js/main.cd436a2e.chunk.js"
+  },
+  {
+    "revision": "6455550597c0233d5909",
+    "url": "/static/js/runtime-main.7a4640f4.js"
+  }
+]);
